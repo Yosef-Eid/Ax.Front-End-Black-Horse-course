ae9e692e36b4c9a7db811c27d1d525d17ba4db97
unknown <yosefeid058@gmail.com>
2024-01-20 22:01:54 +0200

    session6

---
+++ code/script.js
@@ -0,0 +1,79 @@
+
+
+// let 
+// const 
+
+
+// alert('welcome')
+// confirm('gfghhhhh')/
+
+
+// let x = 'enter your name'
+
+// let data = prompt(x)
+
+// console.log();
+
+
+
+
+
+// console.log(total);
+
+// console.log(5+5);
+// console.log(5-5);
+// console.log(5/5);
+// console.log(5*5);
+// console.log(5**5);
+
+// number 1
+// string "" '
+// true and false
+// array 
+// object
+
+// let arr = [
+//     1,
+//     3,
+//     4,
+//     6,
+//     "xxxx",
+//     [1,2,3,4],
+//     {
+//         name:'ali',
+//         age:32,
+//         phone:1234567,
+//         arr:[1, 'ahmed']
+//     }
+// ]
+// // console.log(arr);
+
+// let obj = 
+
+// console.log(obj.name);
+// console.log(obj.phone);
+// console.log(obj.arr[0]);
+
+
+// let names = prompt("enter name")
+// let age = prompt("enter age")
+// let phone = prompt("enter phone")
+
+
+// let data = {
+//     nameUser:names,
+//     age:age,
+//     phone:phone,
+// }
+
+// console.log(data);
+
+
+// let x = 3
+// console.log(x);
+
+// x = 4
+// console.log(4);
+// x--
+// console.log(x);
+
---
+++ sessions/session6/script.js
@@ -0,0 +1,109 @@
+
+
+
+// variables
+
+// let x = 5
+// const y = 5
+
+
+// Arithmetic operators العمليات الحسابية
+// console.log(5+5);
+// console.log(5-5);
+// console.log(5/5);
+// console.log(5*5);
+// console.log(5**5);
+
+// let x = 10.5
+// let y = 10
+
+// let z = x + y
+
+// input & output 
+// alert();
+// confirm();
+// prompt();
+
+
+// let yy =  prompt('number')
+
+// console.log(Number(xx) + Number(yy));
+
+
+// console.log('5' + '5');
+
+
+
+
+// data type
+
+// 12344                   // number
+// 'staring',"string"      // string
+// true, false;            // boolean
+// ['بتشيل اي نوع داتا']  // array
+// {kay:'value'}           // object
+
+
+// 1- number  1 2 3 5
+// 2- string  ''  ""
+// 3- boolean true false 
+// 4- array
+// 5- object
+
+
+// array
+// let arr = ['ahmed', 1, 2, 3, true, false, [1, 2, 3, 'trdfnm'], 'end']
+
+// console.log(arr[6].length);
+
+// let len = arr.length
+// console.log(arr[7]);
+
+// let nameUser = prompt('name')
+// let email = prompt('email')
+// let phone = prompt('phone')
+// let age  = prompt('age')
+// let city = prompt('city')
+
+// let datUser = {
+//     name:nameUser,
+//     email:email,
+//     phon:phone,
+//     age:age,
+//     city:city
+// }
+
+// console.log(datUser);
+
+
+// Logical operators عمليات التسجيل 
+// let x = 7
+// console.log(x);
+
+// x = 10
+// console.log(x);
+
+// x += 1
+// console.log(x);
+
+// x -= 1
+// console.log(x);
+
+// x++
+// console.log(x);
+
+// x --
+// console.log(x);
+
+
+
+//Assignment operators عمليات المقارنه
+
+// let x = 5
+
+// console.log(x > 5);
+// console.log(x < 5);
+// console.log(x >= 5);
+// console.log(x <= 5);
+// console.log(x == 5);
+

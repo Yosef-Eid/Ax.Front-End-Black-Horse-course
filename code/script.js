6feba49cb3f6c7a5ee3ead33b0a70250d4974f49
unknown <yosefeid058@gmail.com>
2024-01-27 11:42:37 +0200

    session6

--- code/script.js
+++ code/script.js
@@ -1,79 +1,19 @@
 
+// const namesArray = ["ali", "ahmed", "khalid", "omar", 'fsd', 'wef', 'ewrsfd', 'zcvzxc'];
 
-// let 
-// const 
+// let inp = document.getElementById("inp");
+// console.log(inp);
 
-
-// alert('welcome')
-// confirm('gfghhhhh')/
-
-
-// let x = 'enter your name'
-
-// let data = prompt(x)
-
-// console.log();
-
-
-
-
-
-// console.log(total);
-
-// console.log(5+5);
-// console.log(5-5);
-// console.log(5/5);
-// console.log(5*5);
-// console.log(5**5);
-
-// number 1
-// string "" '
-// true and false
-// array 
-// object
-
-// let arr = [
-//     1,
-//     3,
-//     4,
-//     6,
-//     "xxxx",
-//     [1,2,3,4],
-//     {
-//         name:'ali',
-//         age:32,
-//         phone:1234567,
-//         arr:[1, 'ahmed']
+// inp.oninput = (e) => {
+//     for(let i = 0; i < namesArray.length; i++) {
+   
+//     if(inp.value !== ''){
+//         if(namesArray[i].indexOf(e.target.value) !== -1){
+//             let names = document.createElement('p')
+//             names.innerHTML = namesArray[i]
+//             document.body.after(names)
+//         }
 //     }
-// ]
-// // console.log(arr);
-
-// let obj = 
-
-// console.log(obj.name);
-// console.log(obj.phone);
-// console.log(obj.arr[0]);
-
-
-// let names = prompt("enter name")
-// let age = prompt("enter age")
-// let phone = prompt("enter phone")
-
-
-// let data = {
-//     nameUser:names,
-//     age:age,
-//     phone:phone,
+//     }
 // }
-
-// console.log(data);
-
-
-// let x = 3
-// console.log(x);
-
-// x = 4
-// console.log(4);
-// x--
-// console.log(x);
 
